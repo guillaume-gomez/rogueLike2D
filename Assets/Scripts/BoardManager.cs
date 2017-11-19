26102885fc66fc493942558dd3e49b608c2a375d
guillaume-gomez <guillaume.gomez846@gmail.com>
2017-11-19 22:16:57 +0000

    add unit mechanics

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -49,9 +49,9 @@ public class BoardManager : MonoBehaviour
     {
         boardHolder = new GameObject("Board").transform;
 
-        for(int x = -1; x < columns + 1; ++x) 
+        for(int x = -1; x < columns + 1; x++) 
         {
-            for(int y = -1; y < rows - 1; ++y)
+            for(int y = -1; y < rows + 1; y++)
             {
                 GameObject toInstanciate = floorTiles[Random.Range(0, floorTiles.Length)];
                 if(x == -1 || x == columns || y == -1 || y == rows)
